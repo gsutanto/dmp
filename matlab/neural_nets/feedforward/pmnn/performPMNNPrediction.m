7af6ef64ec5fd69857caf3680715589c41e29ddf
gsutanto <gsutanto@usc.edu>
2017-10-19 18:33:38 -0700

    pre-load TF PMNN params into cells before computation (hopefully for faster computation)

--- matlab/neural_nets/feedforward/pmnn/performPMNNPrediction.m
+++ matlab/neural_nets/feedforward/pmnn/performPMNNPrediction.m
@@ -1,4 +1,4 @@
-function [ output, layer_cell ] = performPMNNPrediction( neural_net_info, dataset, normalized_phase_kernels )
+function [ output, layer_cell, varargout ] = performPMNNPrediction( neural_net_info, dataset, normalized_phase_kernels )
     % Author        : Giovanni Sutanto
     % Date          : April 2017
     % Description   :
@@ -10,10 +10,17 @@ function [ output, layer_cell ] = performPMNNPrediction( neural_net_info, datase
     assert(size(NN_topology, 1) == 1,             'NN_topology must be a row vector!');
     assert(size(NN_activation_func_list, 1) == 1, 'NN_activation_func_list must be a row vector!');
     assert(size(NN_topology, 2) == size(NN_activation_func_list, 2), 'NN_topology must have the same size as NN_activation_func_list!');
-    NN_model_params_filepath= neural_net_info.filepath;
-    load(NN_model_params_filepath);
     N_layers    = size(NN_topology, 2);
     D_output    = NN_topology(1, N_layers);
+    if (isfield(neural_net_info, 'params') == 0)
+        NN_model_params_filepath= neural_net_info.filepath;
+        load(NN_model_params_filepath);
+        is_params_given         = 0;
+        neural_net_info.params.weights_cell     = cell(N_layers-1, D_output);
+        neural_net_info.params.biases_cell      = cell(N_layers-2, D_output);
+    else
+        is_params_given         = 1;
+    end
     
     layer_cell  = cell(D_output, N_layers);
     for layer_idx = 1:N_layers
@@ -23,14 +30,20 @@ function [ output, layer_cell ] = performPMNNPrediction( neural_net_info, datase
             if (layer_idx == 1) % Input Layer
                 layer_cell{dim_out, layer_idx}  = dataset;
             else
-                layer_dim_ID = [layer_name,'_',num2str(dim_out-1)];
-                var_scope   = [NN_name, '_', layer_dim_ID];
                 N_data  = size(layer_cell{dim_out, layer_idx-1}, 1);
-                weights_loading_command_string  = ['weights = ', var_scope, '_weights;'];
-                eval(weights_loading_command_string);
+                if (is_params_given == 0)
+                    layer_dim_ID = [layer_name,'_',num2str(dim_out-1)];
+                    var_scope   = [NN_name, '_', layer_dim_ID];
+                    weights_loading_command_string      = ['neural_net_info.params.weights_cell{layer_idx-1, dim_out} = ', var_scope, '_weights;'];
+                    eval(weights_loading_command_string);
+                end
+                weights         = neural_net_info.params.weights_cell{layer_idx-1, dim_out};
                 if (layer_idx <= N_layers - 1)  % ALL Hidden Layer (has weights and biases)
-                    biases_loading_command_string   = ['biases  = ', var_scope, '_biases;'];
-                    eval(biases_loading_command_string);
+                    if (is_params_given == 0)
+                        biases_loading_command_string   = ['neural_net_info.params.biases_cell{layer_idx-1, dim_out}  = ', var_scope, '_biases;'];
+                        eval(biases_loading_command_string);
+                    end
+                    biases      = neural_net_info.params.biases_cell{layer_idx-1, dim_out};
                 
                     layer_cell{dim_out, layer_idx}  = (layer_cell{dim_out, layer_idx-1} * weights) + repmat(biases, N_data, 1);
                 else                            % Output Layer (has NO biases)
@@ -55,4 +68,8 @@ function [ output, layer_cell ] = performPMNNPrediction( neural_net_info, datase
         end
     end
     output  = cell2mat((layer_cell(:, N_layers))');
+    
+    if (nargout > 2)
+        varargout(1)    = {neural_net_info};
+    end
 end
