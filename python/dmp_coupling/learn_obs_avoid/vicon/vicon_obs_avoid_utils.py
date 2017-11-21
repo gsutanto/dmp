44cc5da9699940764bf1768fcc2950ff0e95189b
gsutanto <gsutanto@usc.edu>
2017-11-21 07:03:03 -0800

    started vicon data processing on py

---
+++ python/dmp_coupling/learn_obs_avoid/vicon/vicon_obs_avoid_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 19:00:00 2017
+
+@author: gsutanto
+"""
+
+import numpy as np
+import os
+import sys
+import copy
+import glob
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../utilities/'))
+from DataIO import *
+
+def prepareDemoDatasetLOAVicon():
+    trajs_extraction_version = 2
+    dir_path = '../../../../data/dmp_coupling/learn_obs_avoid/static_obs/data_multi_demo_vicon_static/'
+    
+    data_global_coord = {}
+    data_global_coord["baseline"] = 
+    
+    return data_global_coord
