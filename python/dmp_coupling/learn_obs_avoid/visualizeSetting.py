b48a4542634bbebb7c478db4dc21794d999c7f8a
gsutanto <gsutanto@usc.edu>
2017-12-19 16:18:15 +0100

    visualized obstacle vicon markers on visualizeSetting.py

---
+++ python/dmp_coupling/learn_obs_avoid/visualizeSetting.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 19:00:00 2017
+
+@author: gsutanto
+"""
+
+import numpy as np
+import os
+import sys
+import copy
+import time
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import glob
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../dmp_coupling/learn_obs_avoid/vicon/'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../utilities/'))
+from utilities import *
+from vicon_obs_avoid_utils import *
+from plot_sphere import *
+
+def visualizeSetting(setting_no):
+    vicon_marker_radius             = 15.0/2000.0   # in meter
+    critical_position_marker_radius = 30/1000.0     # in meter
+    
+    D                               = 3
+    
+    ## Data Creation/Loading
+    
+    data_dirpath_candidates = ['./learn_obs_avoid_pmnn_vicon_data/']
+    filename = 'data_multi_demo_vicon_static_global_coord.pkl'
+    data_filepath = ''
+    for data_dirpath_candidate in data_dirpath_candidates:
+        if (os.path.isfile(data_dirpath_candidate + filename)):
+            data_filepath = data_dirpath_candidate + filename
+            break
+    
+    if (data_filepath == ''):
+        data_global_coord = prepareDemoDatasetLOAVicon()
+        saveObj(data_global_coord, (data_dirpath_candidates[0] + filename))
+    else:
+        data_global_coord = loadObj(data_filepath)
+    
+    ## end of Data Creation/Loading
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_aspect("equal")
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    if (setting_no > 0):    # indexing of the settings start at 1!
+        for op in range(data_global_coord['obs_avoid'][0][setting_no-1].shape[0]):
+            plot_sphere(ax, vicon_marker_radius, 
+                        data_global_coord['obs_avoid'][0][setting_no-1][op,0], 
+                        data_global_coord['obs_avoid'][0][setting_no-1][op,1], 
+                        data_global_coord['obs_avoid'][0][setting_no-1][op,2], 'r')
+    
+    return None
+
+if __name__ == "__main__":
+    visualizeSetting(103)
