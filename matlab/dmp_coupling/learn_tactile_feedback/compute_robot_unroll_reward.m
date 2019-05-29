21604b31267e9c8c4d703825bed2223c6f0254db
gsutanto <gsutanto@usc.edu>
2019-05-29 00:30:26 -0700

    extracting rewards from datafiles

--- matlab/dmp_coupling/learn_tactile_feedback/compute_robot_unroll_reward.m
+++ matlab/dmp_coupling/learn_tactile_feedback/compute_robot_unroll_reward.m
@@ -14,7 +14,10 @@ date                    = '20190508';
 additional_description  = '';
 experiment_name         = [date,'_',specific_task_type,'_correctable',additional_description];
 
-env_setting_name        = 'p10';
+homepath                = getenv('HOME');
+N_total_sense_dimensionality    = 45;
+
+env_setting_name        = 'p5';
 unroll_types            = {'b', 'c'};   % 'b' = baseline; 'c' = coupled
 prim2_Rewards           = cell(size(unroll_types));
 prim3_Rewards           = cell(size(unroll_types));
@@ -22,7 +25,7 @@ prim2_aveRewards        = cell(size(unroll_types));
 prim3_aveRewards        = cell(size(unroll_types));
 disp(['Environment Setting ', env_setting_name]);
 for uti = 1:length(unroll_types)
-    data_dir_path       = ['/home/zhesu_local/Desktop/dmp_robot_unroll_results/scraping/',experiment_name,'/robot/',env_setting_name,'/',unroll_types{1,uti}];
+    data_dir_path       = [homepath,'/Desktop/dmp_robot_unroll_results/scraping/',experiment_name,'/robot/',env_setting_name,'/',unroll_types{1,uti}];
     data_files          = dir([data_dir_path,'/d*']);
     N_files             = length(data_files);
     prim2_Rewards{1,uti}= zeros(N_files, 1);
@@ -32,8 +35,8 @@ for uti = 1:length(unroll_types)
         n_file          = n_file + 1;
         data_file_path  = [data_dir_path,'/',data_file.name];
         [D,vars,freq]   = clmcplot_convert(data_file_path);
-        vector          = cell(1,19);
-        for i = 1:19
+        vector          = cell(1,N_total_sense_dimensionality);
+        for i = 1:N_total_sense_dimensionality
             vector{1,i} = clmcplot_getvariables(D, vars, {strcat('X_vector_',num2str(i-1,'%02d'))});
         end
         X_vector        = horzcat(vector{:});
@@ -45,8 +48,8 @@ for uti = 1:length(unroll_types)
         prim2_X_vector  = X_vector(prim2_indices,:);
         prim3_X_vector  = X_vector(prim3_indices,:);
 
-        prim2_Rewards{1,uti}(n_file,1)  = norm(prim2_X_vector);
-        prim3_Rewards{1,uti}(n_file,1)  = norm(prim3_X_vector);
+        prim2_Rewards{1,uti}(n_file,1)  = -norm(prim2_X_vector);
+        prim3_Rewards{1,uti}(n_file,1)  = -norm(prim3_X_vector);
     end
     prim2_aveRewards{1,uti}  = mean(prim2_Rewards{1,uti});
     prim3_aveRewards{1,uti}  = mean(prim3_Rewards{1,uti});
--- python/dmp_coupling/learn_tactile_feedback/reinforcement_learning/rl_tactile_fb.py
+++ python/dmp_coupling/learn_tactile_feedback/reinforcement_learning/rl_tactile_fb.py
@@ -9,13 +9,29 @@ Created on Fri May 10 17:00:00 2019
 import os
 import sys
 import re
+import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../utilities/'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../utilities/clmcplot/'))
 import utilities as py_util
+import clmcplot_utils as clmcplot_util
 
 catkin_ws_path = py_util.getCatkinWSPath()
 sl_data_path = catkin_ws_path + "/install/bin/arm"
 
-# initialization by removing all SL data files inside sl_data_path
-initial_dfilepaths = [sl_data_path+'/'+f for f in os.listdir(sl_data_path) if re.match(r'd+\d{5,5}$', f)] # match the regex pattern of SL data files
-for initial_dfilepath in initial_dfilepaths:
-    os.remove(initial_dfilepath)
+is_deleting_all_dfiles_at_init = False#True
+N_total_sense_dimensionality = 45
+
+if (is_deleting_all_dfiles_at_init):
+    # initialization by removing all SL data files inside sl_data_path
+    initial_dfilepaths = [sl_data_path+'/'+f for f in os.listdir(sl_data_path) if re.match(r'd+\d{5,5}$', f)] # match the regex pattern of SL data files
+    for initial_dfilepath in initial_dfilepaths:
+        os.remove(initial_dfilepath)
+
+init_new_env_dfilepaths = [sl_data_path+'/'+f for f in os.listdir(sl_data_path) if re.match(r'd+\d{5,5}$', f)] # match the regex pattern of SL data files
+for init_new_env_dfilepath in init_new_env_dfilepaths:
+    print("Processing datafile %s..." % init_new_env_dfilepath)
+    clmcfile = clmcplot_util.ClmcFile(init_new_env_dfilepath)
+    prim_no = clmcfile.get_variables(["ul_curr_prim_no"])[0].T
+    X_vector = np.vstack([clmcfile.get_variables(["X_vector_%02d" % i for i in range(N_total_sense_dimensionality)])]).T
+    assert(X_vector.shape[0] == prim_no.shape[0])
+    assert(X_vector.shape[1] == N_total_sense_dimensionality)
