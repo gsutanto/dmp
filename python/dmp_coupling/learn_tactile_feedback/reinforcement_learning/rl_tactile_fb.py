2ebaad5acf8bb8ccf9e6a1982d004bd6cb8afc6e
gsutanto <gsutanto@usc.edu>
2019-06-20 12:39:37 -0700

    added TODOs, including places where we need to do robot executions (communicate with real-time C++, etc.)

--- python/dmp_coupling/learn_tactile_feedback/reinforcement_learning/rl_tactile_fb.py
+++ python/dmp_coupling/learn_tactile_feedback/reinforcement_learning/rl_tactile_fb.py
@@ -25,14 +25,14 @@ sl_data_dirpath = catkin_ws_path + "/install/bin/arm"
 orig_prims_params_dirpath = "../../../../data/dmp_coupling/learn_tactile_feedback/scraping/learned_prims_params/"
 outdata_dirpath = './'
 
-is_deleting_dfiles = False#True
+is_deleting_dfiles = False#True # TODO (remove this)
 is_smoothing_training_traj_before_learning = True
 is_plotting = True#False
 
 N_total_sense_dimensionality = 45
 N_primitives = 3
-#prims_tbi = [1,2] # 2nd and 3rd primitives are to-be-improved (tbi)
-prims_tbi = [1] # for testing purpose we work on 2nd primitive only as the one to-be-improved (tbi)
+#prims_tbi = [1,2] # TODO (un-comment): 2nd and 3rd primitives are to-be-improved (tbi)
+prims_tbi = [1] # TODO (comment): for testing purpose we work on 2nd primitive only as the one to-be-improved (tbi)
 cost_threshold = [0.0, 18928850.8053, 11066375.797]
 cart_dim_tbi = {}
 cart_dim_tbi["Quaternion"] = np.array([1]) # to-be-improved (tbi): Quaternion DMP, 2nd dimension
@@ -40,7 +40,7 @@ cart_dim_tbi["Quaternion"] = np.array([1]) # to-be-improved (tbi): Quaternion DM
 # not sure if the original (nominal) primitives below is needed or not...:
 #orig_cdmp_params = rl_util.loadPrimsParamsAsDictFromDirPath(orig_prims_params_dirpath, N_primitives)
 
-if (is_deleting_dfiles):
+if (is_deleting_dfiles): # TODO (remove this)
     # initialization by removing all SL data files inside sl_data_dirpath
     py_util.deleteAllCLMCDataFilesInDirectory(sl_data_dirpath)
 
@@ -51,6 +51,12 @@ for prim_tbi in prims_tbi:
     rl_data[prim_tbi] = {}
     it = 0
     
+    if (is_deleting_dfiles): # TODO (remove this)
+        py_util.deleteAllCLMCDataFilesInDirectory(sl_data_dirpath)
+    
+    # TODO: Robot Execution: to evaluate initial cost on current closed-loop behavior 
+    #                        (involving feedback model) by unrolling 3 times and averaging the cost
+    
     # extract initial unrolling results: trajectories, sensor trace deviations, cost
     rl_data[prim_tbi][it] = rl_util.extractUnrollResultsFromCLMCDataFilesInDirectory(sl_data_dirpath, 
                                                                                      N_primitives=N_primitives, 
@@ -59,6 +65,8 @@ for prim_tbi in prims_tbi:
     py_util.saveObj(rl_data, outdata_dirpath+'rl_data.pkl')
     
     while (rl_data[prim_tbi][it]["mean_accum_cost"][prim_tbi] > cost_threshold[prim_tbi]): # while (J > threshold):
+        # convert current closed-loop behavior into an equivalent open-loop behavior on
+        # the current (assumed-static) environment setting
         cdmp_trajs = rl_util.extractCartDMPTrajectoriesFromUnrollResults(rl_data[prim_tbi][it])
         [
          rl_data[prim_tbi][it]["cdmp_params_all_dim_learned"], 
@@ -70,11 +78,13 @@ for prim_tbi in prims_tbi:
         
         py_util.saveObj(rl_data, outdata_dirpath+'rl_data.pkl')
         
-        if (is_deleting_dfiles):
+        if (is_deleting_dfiles): # TODO (remove this)
             py_util.deleteAllCLMCDataFilesInDirectory(sl_data_dirpath)
         
-        # unroll DMP params, and measure cost J'
-        # check (assert?) if J' is closely similar to J?
+        # TODO: Robot Execution: unroll DMP params of the (assumed) equivalent open-loop behavior, 
+        #                        and measure cost J'
+        
+        # TODO: check (assert?) if J' is closely similar to J?
         
         for cart_type_tbi in cart_dim_tbi.keys():
             # set to-be-perturbed DMP params (2nd dimension of orientation) as mean, and define the initial covariance matrix
@@ -83,8 +93,8 @@ for prim_tbi in prims_tbi:
             
             assert False
             
-            # sample K perturbed DMP params from the multivariate normal distribution with mean and cov parameters (and log these K samples of perturbed DMP params)
-            # For Debugging via visualizations: unroll each of these K perturbed DMP params, log the unrolled trajectories, and visualize them as plots (as needed)
-            # save these K perturbed DMP params (one at a time) as text files, to be loaded by C++ program and executed by the robot, to evaluate each of their costs.
-            # summarize these K perturbed DMP params into mean_new and cov_new using PI2 update, based on each of their cost
-            # save mean_new (which is a DMP params by itself) as text files, to be loaded by C++ program and executed by the robot, to evaluate its cost Nu times, to ensure the average cost is really lower than the original one
+            # TODO: sample K perturbed DMP params from the multivariate normal distribution with mean and cov parameters (and log these K samples of perturbed DMP params)
+            # TODO: For Debugging via visualizations: unroll each of these K perturbed DMP params, log the unrolled trajectories, and visualize them as plots (as needed)
+            # TODO: save these K perturbed DMP params (one at a time) as text files, to be loaded by C++ program and executed by the robot, to evaluate each of their costs.
+            # TODO: summarize these K perturbed DMP params into mean_new and cov_new using PI2 update, based on each of their cost
+            # TODO: save mean_new (which is a DMP params by itself) as text files, to be loaded by C++ program and executed by the robot, to evaluate its cost Nu times, to ensure the average cost is really lower than the original one
