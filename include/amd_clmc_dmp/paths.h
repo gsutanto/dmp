7d22285101fcf1af44632efec70fe98ce7d4f9f7
Giovanni Sutanto <gsutanto@usc.edu>
2019-07-22 17:27:17 -0700

    load topology from a single file, instead of hardcoded for tactile fb learning project

--- include/amd_clmc_dmp/paths.h
+++ include/amd_clmc_dmp/paths.h
@@ -13,6 +13,8 @@ std::string get_data_path(const char* relative_path);
 void get_data_path(char * get, const char* relative_path);
 std::string get_matlab_path(const char* relative_path);
 void get_matlab_path(char * get, const char* relative_path);
+std::string get_python_path(const char* relative_path);
+void get_python_path(char * get, const char* relative_path);
 std::string get_plot_path(const char* relative_path);
 void get_plot_path(char * get, const char* relative_path);
 std::string get_rt_errors_path(const char* relative_path);
